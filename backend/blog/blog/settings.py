2d4e2f86f64d9609dbc0fc3a7fb26e13a2debfc5
ErebusZ <azouiten@ostorlab.dev>
2024-09-28 14:59:16 +0100

    Add middleware to handle missing trailing slash

--- backend/blog/blog/settings.py
+++ backend/blog/blog/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Custom middleware
+    "blog.middleware.HandlePostWithoutSlashMiddleware",
 ]
 
 ROOT_URLCONF = "blog.urls"

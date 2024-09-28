184cd5f7f8f41907f2839f2c01882dd6a064e3f3
ErebusZ <azouiten@ostorlab.dev>
2024-09-28 14:00:17 +0100

    Add db settings

--- backend/blog/blog/settings.py
+++ backend/blog/blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,12 @@ WSGI_APPLICATION = "blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog_database",
+        "USER": os.environ.get("SETTINGS_DB_USER", "django_user"),
+        "PASSWORD": os.environ.get("SETTINGS_DB_PASSWORD", "django_password"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 

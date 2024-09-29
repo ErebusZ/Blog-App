a1c6655cd25ce5dacacdcc9b11ebf8b06d1f9c71
ErebusZ <azouiten@ostorlab.dev>
2024-09-29 20:22:02 +0100

    add register view route

--- blog-frontend/src/router/index.ts
+++ blog-frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import type {RouteRecordRaw } from 'vue-router';
 import Home from '@/views/HomeView.vue';
+import Register from '@/views/RegisterView.vue';
 
 
 const routes: Array<RouteRecordRaw> = [
@@ -8,7 +9,12 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Home',
     component: Home,
-  }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+  },
 ];
 
 const router = createRouter({

840317ef3a80b0a1627442c771dfeed8937f77bb
ErebusZ <azouiten@ostorlab.dev>
2024-09-29 11:57:06 +0100

    Add pandatic shemas

---
+++ comments_service/app/api/models.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+
+class Comment(BaseModel):
+    content: str
+    user_id: int
+    post_id: int
+
+
+class CommentIn(BaseModel):
+    content: str
+    user_id: int
+    post_id: int
+
+
+class CommentOut(CommentIn):
+    id: int
+
+
+class CommentUpdate(CommentIn):
+    content: str | None
+    user_id: int | None
+    post_id: int | None

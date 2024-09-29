67ec8f22e0d286258f1a5d33c6e21e927741c20c
ErebusZ <azouiten@ostorlab.dev>
2024-09-29 19:55:54 +0100

    add home view

--- blog-frontend/src/views/HomeView.vue
+++ blog-frontend/src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>Blog Posts</h1>
+    <SearchBar />
+    <BlogPostList />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BlogPostList from '@/components/BlogPostList.vue';
+import SearchBar from '@/components/SearchBar.vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    BlogPostList,
+    SearchBar,
+  },
+});
+</script>

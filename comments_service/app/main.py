b734f9c7a89f3c6dc9534b84117d93b53c76722e
ErebusZ <azouiten@ostorlab.dev>
2024-09-30 17:51:13 +0100

    Add cors headers

--- comments_service/app/main.py
+++ comments_service/app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import db
 
 db.metadata.create_all(db.engine)
 
+origins = [
+    "http://localhost:3000",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 async def startup():
@@ -13,5 +27,3 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await db.database.disconnect()
-
-

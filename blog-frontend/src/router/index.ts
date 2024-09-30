ed4d77c9bdda3f96fbd6bfa953af41f5906173ef
ErebusZ <azouiten@ostorlab.dev>
2024-09-30 21:52:14 +0100

    add register route

--- blog-frontend/src/router/index.ts
+++ blog-frontend/src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomeView.vue";
 import Login from "../views/LoginView.vue";
+import Register from "../views/RegisterView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 
 const routes = [
   { path: "/", component: Home, name: "Home" },
   { path: "/login", component: Login },
+  { path: "/register", component: Register },
   { path: "/:pathMatch(.*)*", component: NotFoundView },
 ];
 

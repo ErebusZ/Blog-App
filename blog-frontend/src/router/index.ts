fbf67aca246335b24cbbfbc4842364c83c6cb2f8
ErebusZ <azouiten@ostorlab.dev>
2024-09-30 21:25:59 +0100

    add not found

--- blog-frontend/src/router/index.ts
+++ blog-frontend/src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomeView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const routes = [
   { path: "/", component: Home, name: "Home" },
+  { path: "/:pathMatch(.*)*", component: NotFoundView },
 ];
 
 const router = createRouter({
---
+++ blog-frontend/src/views/NotFoundView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-4xl font-bold">404 - Not Found</h1>
+    <p class="mt-4">The page you are looking for does not exist.</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style scoped></style>
